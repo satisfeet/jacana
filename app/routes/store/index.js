ea639af8d4c2c98e77ad3197931f5955854b068e
Bodo Kaiser <i@bodokaiser.io>
2013-12-28 10:59:28 +0100

    updated app/routes, see issue #3
    seperated order list from app/routes/store/views/sidebar
    
    normalized helper functions
    
    improved event binding to product list

--- app/routes/store/index.js
+++ app/routes/store/index.js
@@ -4,27 +4,23 @@ module.exports = function(app) {
         require('./views')(app, context);
         require('./models')(app, context);
 
-        context.events.on('orders:list', function() {
-            context.orders.find();
-        });
-        context.events.on('orders:find', function(orders) {
+        context.orders.on('find', function(orders) {
             context.sidebar.listOrders(orders);
         });
-        context.events.on('orders:push', function(product) {
-            context.orders.persist(product);
-        });
-
-        context.events.on('products:list', function() {
-            context.products.find();
-        });
-        context.events.on('products:find', function(products) {
+        context.products.on('find', function(products) {
             context.content.listProducts(products);
         });
-        context.events.on('products:show', function(product) {
+        
+        context.sidebar.on('click:orders', function() {
+            context.orders.find();
+        });
+        context.content.on('click:product', function(product) {
+            context.content.selectProduct(product);
             context.sidebar.showProduct(product);
         });
-
-        context.events.emit('products:list');
+        context.sidebar.on('click:product:add', function(product) {
+            context.orders.persist(product);
+        });
     });
 
 };
---
+++ app/routes/store/views/sidebar/product/info.js
@@ -0,0 +1,35 @@
+var util   = require('util');
+var events = require('events');
+var domify = require('domify');
+
+var template = require('../../../../../../usr/views/store/sidebar/product/info.html');
+
+function ProductInfo(element) {
+    this.element = element || domify(template);
+
+    bindToButtonClickEvent(this.element, this);
+
+    events.EventEmitter.call(this);
+}
+
+util.inherits(ProductInfo, events.EventEmitter);
+
+ProductInfo.prototype.show = function(product) {
+    this.model = product;
+
+    if (this.element.dataset.id !== product._id) {
+        this.element.dataset.id = product._id;
+        this.element.querySelector('p').innerText = product.description;
+    }
+
+    return this;
+};
+
+module.exports = ProductInfo;
+
+function bindToButtonClickEvent(element, view) {
+    element.querySelector('button[name="add"]')
+        .addEventListener('click', function(e) {
+            view.emit('click:add', view.model);
+        });
+}
