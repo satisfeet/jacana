94d67b63f3d7dceb4eb90fb5d307bb3c9800d7db
Bodo Kaiser <i@bodokaiser.io>
2013-12-29 13:31:21 +0100

    updated test/lib/static, reenabled javascripts/build.js

--- test/lib/static/javascripts.js
+++ test/lib/static/javascripts.js
@@ -6,8 +6,7 @@ module.exports = function(app) {
 
     describe('/javascripts', function() {
 
-        // BUG: parse error on socket
-        xit('build.js', function(done) {
+        it('build.js', function(done) {
             supertest(app).get('/javascripts/build.js')
                 .expect(200, done);
         });
