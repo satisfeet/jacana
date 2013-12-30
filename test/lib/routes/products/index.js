efe518657403059c77b006b039930a1f8f8cde6e
Bodo Kaiser <i@bodokaiser.io>
2013-12-30 16:05:52 +0100

    updated lib/models, added variation map to Product

--- test/lib/routes/products/index.js
+++ test/lib/routes/products/index.js
@@ -3,7 +3,14 @@ module.exports = function(app) {
     var mockup = {
         product: {
             name: "socks",
-            description: "bla bla bla"
+            image: {
+                path: '/images/products/new.jpg'
+            },
+            description: "bla bla bla",
+            variations: {
+                colors: ['blue', 'red'],
+                sizes: ['38-40', '40-42']
+            }
         }
     };
 
