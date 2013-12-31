f606b9e7d66950b1578ece32dd5af8a1fdbbe7ed
Bodo Kaiser <i@bodokaiser.io>
2013-12-31 15:20:48 +0100

    updated app/routes/store, removedContentView

--- app/routes/store/index.js
+++ app/routes/store/index.js
@@ -1,4 +1,5 @@
-var ContentView = require('./content');
+var ProductListView = require('./product/list');
+
 var SidebarView = require('./sidebar');
 
 var template = require('views/store/layout.html');
@@ -10,54 +11,54 @@ module.exports = function(app) {
             context.element.innerHTML = template;
         }
 
-        context.contentView = createContentView(context);
-        context.sidebarView = createSidebarView(context);
+        var sidebarView = createSidebarView(context);
+        var productListView = createProductListView(context);
  
         context.productManager.find(null, function(err, products) {
-            context.contentView.listProducts(products);
+            productListView.list(products);
         });
         
-        context.sidebarView.on('order:show', function() {
+       sidebarView.on('order:show', function() {
             context.orderManager.findOne(null, function(err, order) {
-                if (order) return context.sidebarView.showOrder(order);
+                if (order) return sidebarView.showOrder(order);
 
                 context.orderManager.create(null, function(order) {
-                    context.sidebarView.showOrder(order);
+                    sidebarView.showOrder(order);
                 });
             });
         });
         
-        context.sidebarView.on('order:push', function(product, variations) {
+        sidebarView.on('order:push', function(product, variations) {
             context.orderManager.findOne(null, function(err, order) {
                 if (order.products.indexOf(product) !== -1) return;
                 
                 order.pushProduct(product, variations);
          
                 context.orderManager.update(order, function(err, order) {
-                    context.sidebarView.showOrder(order);
+                    sidebarView.showOrder(order);
                 });
             });
         });
         
-        context.sidebarView.on('order:remove', function(product) {
+        sidebarView.on('order:remove', function(product) {
             context.orderManager.findOne(null, function(err, order) {
                 var index = order.products.indexOf(product);
 
                 order.products.splice(index, 1);
 
                 context.orderManager.update(order, function(err, order) {
-                    context.sidebarView.showOrder(order);
+                    sidebarView.showOrder(order);
                 });
             });
         });
 
-        context.sidebarView.on('order:proceed', function() {
+        sidebarView.on('order:proceed', function() {
             context.orderManager.findOne(null, function(err, order) {
-                context.sidebarView.showOrderCheckout(order);
+                sidebarView.showOrderCheckout(order);
             });
         });
 
-        context.sidebarView.on('order:submit', function(customer) {
+        sidebarView.on('order:submit', function(customer) {
             context.orderManager.findOne(null, function(err, order) {
                 order.customer = customer;
 
@@ -65,28 +66,15 @@ module.exports = function(app) {
             });
         });
         
-        context.contentView.on('product:show', function(product) {
+        productListView.on('show', function(product) {
             context.productManager.findOne(product, function(err, product) {
-                context.contentView.selectProduct(product);
-                context.sidebarView.showProduct(product);
+                productListView.select(product);
+                sidebarView.showProduct(product);
             });
         });
     });
 
 };
-
-function createContentView(context) {
-    var element = context.element.querySelector('#content');
-
-    var contentView = new ContentView(element);
-
-    if (!element) {
-        context.element.querySelector('.row')
-            .appendChild(contentView.element);
-    }
-
-    return contentView;
-}
 
 function createSidebarView(context) {
     var element = context.element.querySelector('#sidebar');
@@ -99,4 +87,17 @@ function createSidebarView(context) {
     }
 
     return sidebarView;
+}
+
+function createProductListView(context) {
+    var element = context.element.querySelector('#product-list');
+
+    var productListView = new ProductListView(element);
+
+    if (!element) {
+        context.element.querySelector('#content-inner')
+            .appendChild(productListView.element);
+    }
+
+    return productListView;
 }
