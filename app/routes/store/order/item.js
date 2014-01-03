74ae7de6e5ca6513491fee52b725d3eac5df18ab
Bodo Kaiser <i@bodokaiser.io>
2014-01-03 12:11:37 +0100

    updated app/routes/store, using swig as template engine

--- app/routes/store/order/item.js
+++ app/routes/store/order/item.js
@@ -1,14 +1,14 @@
-var domify   = require('domify');
-var reactive = require('reactive');
+var swig   = require('swig');
+var domify = require('domify');
 
 var template = require('views/store/order/item.html');
 
 function OrderItem(model) {
-    this.element = domify(template);
+    this.element = domify(swig.render(template, {
+        locals: { product: model }
+    }));
 
     bindToClickEvents(this.element, model, this);
-
-    reactive(this.element, model, this);
 }
 
 OrderItem.prototype.remove = function() {
@@ -19,9 +19,9 @@ module.exports = OrderItem;
 
 function bindToClickEvents(element, model, view) {
     element = element.querySelector('button.close');
+    
     element.addEventListener('click', function(e) {
         model.remove();
-
         view.remove();
     });
 }
--- app/routes/store/product/index.js
+++ app/routes/store/product/index.js
@@ -1,18 +1,15 @@
 var util     = require('util');
+var swig     = require('swig');
 var domify   = require('domify');
-var reactive = require('reactive');
 
 var template = require('views/store/product/index.html');
 
 function Product(model) {
-    this.element = domify(template);
+    this.element = domify(swig.render(template, {
+        locals: { product: model }
+    }));
 
     bindToSubmitEvents(this.element, model, this);
-    
-    buildSizeFormGroup(this.element, model, this);
-    buildColorFormGroup(this.element, model, this);
-
-    reactive(this.element, model, this);
 }
 
 module.exports = Product;
@@ -23,50 +20,16 @@ function bindToSubmitEvents(element, model, view) {
     element.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        var size = element.querySelector('input[name="size"]:checked');
-        var color = element.querySelector('input[name="color"]:checked');
-        var quantity = element.querySelector('input[name="quantity"]');
+        var size = element.querySelector('#sizes :checked');
+        var color = element.querySelector('#colors :checked');
+        var quantity = element.querySelector('#quantity');
 
         model.emit('order', {
-            size: size.value,
-            color: color.value,
+            variation: {
+                size: size.value,
+                color: color.value,
+            },
             quantity: quantity.value
         });
-    });
-}
-
-function buildSizeFormGroup(element, model, view) {
-    element = element.querySelector('#sizes');
-
-    while (element.lastElementChild) {
-        element.lastElementChild.remove();
-    }
-
-    model.variations.sizes.forEach(function(size) {
-        var template = '';
-        template += '<label class="radio-inline">';
-        template += '<input type="radio" name="size" value="%s" data-value="variations.size" />';
-        template += '%s';
-        template += '</label>';
-    
-        element.appendChild(domify(util.format(template, size, size)));
-    });
-}
-
-function buildColorFormGroup(element, model, view) {
-    element = element.querySelector('#colors');
-
-    while (element.lastElementChild) {
-        element.lastElementChild.remove();
-    }
-
-    model.variations.colors.forEach(function(color) {
-        var template = '';
-        template += '<label class="radio-inline">';
-        template += '<input type="radio" name="color" value="%s" />';
-        template += '%s';
-        template += '</label>';
-    
-        element.appendChild(domify(util.format(template, color, color)));
     });
 }
