61b4f25e8871ea464f0f57dedab842b3c500c1f4
Bodo Kaiser <i@bodokaiser.io>
2014-01-08 13:07:00 +0100

    updated test/bin, added models
    created test/bin/models/orders/index.js
    
    created test/bin/models/orders/persist.js

---
+++ test/bin/models/orders/persist.js
@@ -0,0 +1,13 @@
+var should = require('should');
+
+module.exports = function(bin) {
+
+    describe('#persist', function() {
+    
+        it('should persist', function() {
+        
+        });
+    
+    });
+
+};
