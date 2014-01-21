349d596f5b5b361245fdf5dd4f18da965052239e
Bodo Kaiser <i@bodokaiser.io>
2014-01-21 17:15:23 +0100

    fixed issue #select in app/routes/store/product/list

--- app/routes/store/order/item.js
+++ app/routes/store/order/item.js
@@ -19,7 +19,7 @@ module.exports = OrderItem;
 
 function bindToClickEvents(element, model, view) {
     element = element.querySelector('button.close');
-    
+
     element.addEventListener('click', function(e) {
         model.remove();
         view.remove();
