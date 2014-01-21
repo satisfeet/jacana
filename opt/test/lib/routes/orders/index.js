67e39840032047f509076209388e51bb5dbe5044
Bodo Kaiser <i@bodokaiser.io>
2014-01-21 19:57:06 +0100

    merged specific tests in test/lib/routes
    - merged app/* into app/index.js
    - merged orders/* into orders/index.js
    - merged products/* into products/index.js

--- opt/test/lib/routes/orders/index.js
+++ opt/test/lib/routes/orders/index.js
@@ -1,11 +1,67 @@
+var should    = require('should');
+var supertest = require('supertest');
+
 module.exports = function(app, mockup) {
 
-  require('./search')(app, mockup);
+  describe('GET /orders', function() {
 
-  require('./create')(app, mockup);
+    it('should respond empty array', function(done) {
+      supertest(app).get('/orders')
+        .expect(200, function(err, res) {
+          if (err) throw err;
 
-  require('./display')(app, mockup);
+          res.body.should.be.an.Array;
 
-  require('./destroy')(app, mockup);
+          done();
+        });
+    });
+
+  });
+
+  describe('POST /orders', function() {
+
+    var order = mockup.orders[0];
+
+    it('should respond order', function(done) {
+      // needs this timeout as it sends an email
+      this.timeout(5000);
+
+      supertest(app).post('/orders').send(order)
+        .expect(200, function(err, res) {
+          if (err) throw err;
+
+          mockup.order = res.body;
+
+          res.body.should.have.properties(Object.keys(order));
+
+          done();
+        });
+    });
+
+  });
+
+  describe('GET /orders/:id', function() {
+
+    it('should respond order', function(done) {
+      supertest(app).get('/orders/' + mockup.order._id)
+        .expect(200, function(err, res) {
+          if (err) throw err;
+
+          res.body.should.eql(mockup.order);
+
+          done();
+        });
+    });
+
+  });
+
+  describe('DELETE /orders/:id', function() {
+
+    it('should respond OK', function(done) {
+      supertest(app).del('/orders/' + mockup.order._id)
+        .expect(200, done);
+    });
+
+  });
 
 };
--- opt/test/lib/routes/products/index.js
+++ opt/test/lib/routes/products/index.js
@@ -1,13 +1,76 @@
+var should    = require('should');
+var supertest = require('supertest');
+
 module.exports = function(app, mockup) {
 
-  require('./search')(app, mockup);
+  describe('GET /products', function() {
 
-  require('./create')(app, mockup);
+    it('should respond empty array', function(done) {
+      supertest(app).get('/products')
+      .expect(200, function(err, res) {
+        if (err) throw err;
 
-  require('./display')(app, mockup);
+        res.body.should.be.an.Array;
 
-  require('./upgrade')(app, mockup);
+        done();
+      });
+    });
 
-  require('./destroy')(app, mockup);
+  });
+
+  describe('POST /products', function() {
+
+    var product = mockup.products[0];
+
+    it('should respond product', function(done) {
+      supertest(app).post('/products').send(product)
+        .expect(200, function(err, res) {
+          if (err) throw err;
+
+          mockup.product = res.body;
+
+          res.body.should.have.properties(Object.keys(product));
+
+          done();
+        });
+    });
+
+  });
+
+  describe('GET /products/:id', function() {
+
+    it('should respond product', function(done) {
+      supertest(app).get('/products/' + mockup.product._id)
+      .expect(200, function(err, res) {
+        if (err) throw err;
+
+        res.body.should.eql(mockup.product);
+
+        done();
+      });
+    });
+
+  });
+
+  describe('PUT /products/:id', function() {
+
+    it('should respond OK', function(done) {
+      mockup.product.description += 'blub';
+
+      supertest(app).put('/products/' + mockup.product._id)
+      .send(mockup.product)
+      .expect(200, done);
+    });
+
+  });
+
+  describe('DELETE /products/:id', function() {
+
+    it('should respond OK', function(done) {
+      supertest(app).del('/products/' + mockup.product._id)
+      .expect(200, done);
+    });
+
+  });
 
 };
