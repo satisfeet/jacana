24fb46fe54c9fe7de2ce4b78e6ec0f11409716d7
Bodo Kaiser <i@bodokaiser.io>
2014-01-21 19:57:06 +0100

    merged specific tests in test/lib/routes
    - merged app/* into app/index.js
    - merged orders/* into orders/index.js
    - merged products/* into products/index.js

--- opt/test/lib/routes/app/index.js
+++ opt/test/lib/routes/app/index.js
@@ -1,11 +1,42 @@
+var should    = require('should');
+var supertest = require('supertest');
+
 module.exports = function(app) {
 
-  require('./main')(app);
+  describe('GET /', function() {
 
-  require('./about')(app);
+    it('should response OK', function(done) {
+      supertest(app).get('/')
+      .expect(200, done);
+    });
 
-  require('./legal')(app);
+  });
 
-  require('./store')(app);
+  describe('GET /about', function() {
+
+    it('should response OK', function(done) {
+      supertest(app).get('/about')
+      .expect(200, done);
+    });
+
+  });
+
+  describe('GET /legal', function() {
+
+    it('should response OK', function(done) {
+      supertest(app).get('/legal')
+      .expect(200, done);
+    });
+
+  });
+
+  describe('GET /store', function() {
+
+    it('should respond OK', function(done) {
+      supertest(app).get('/store')
+      .expect(200, done);
+    });
+
+  });
 
 };
