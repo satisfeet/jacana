81783b359e033ee8f1f1ae76da2da1421011b0c5
Bodo Kaiser <i@bodokaiser.io>
2014-01-22 21:10:42 +0100

    improved form views in app/routes/store
    - iterating over form.elements instead of form.querySelector

--- app/routes/store/product/index.js
+++ app/routes/store/product/index.js
@@ -8,13 +8,13 @@ var template = require('views/store/product/index.html');
 function Product(element, model) {
   this.element = element.querySelector('#product-info');
 
-  setup(this.element, model, this);
-  submit(this.element, model, this);
+  setupElement(this.element, model, this);
+  bindToSubmit(this.element, model, this);
 }
 
 module.exports = Product;
 
-function setup(element, model, view) {
+function setupElement(element, model, view) {
   // render template if this is the wrong product id
   if (!view.element || view.element.dataset.id !== model.get('_id')) {
     view.element = domify(swig.render(template, {
@@ -23,18 +23,17 @@ function setup(element, model, view) {
   }
 }
 
-function submit(element, model, view) {
+function bindToSubmit(element, model, view) {
   var form = element.querySelector('form')
 
   form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     var attributes = {};
-
-    // TODO: lodash.forEach(form.elements)
-    var elements = element.querySelectorAll('input:checked');
-    lodash.forEach(elements, function(element) {
-      attributes[element.name] = element.value;
+    lodash.forEach(form.elements, function(element) {
+      if (element instanceof HTMLInputElement && element.checked) {
+        attributes[element.name] = element.value;
+      }
     });
 
     var quantity = element.querySelector('#quantity');
