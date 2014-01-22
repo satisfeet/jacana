d95e16566416b8a0c2fd0af6a2d7eb50732d588b
Bodo Kaiser <i@bodokaiser.io>
2014-01-22 21:48:18 +0100

    rewrote app/routes/store/order
    - merged checkout to order/customer
    - moved order/index to order/list

--- app/routes/store/order/customer/index.js
+++ app/routes/store/order/customer/index.js
@@ -2,49 +2,49 @@ var swig   = require('swig');
 var domify = require('domify');
 var lodash = require('lodash');
 
-var template = require('views/store/checkout/index.html');
+var template = require('views/store/order/customer/index.html');
 
-function Checkout(element, model) {
+function Customer(element, model) {
   this.element = element.querySelector('#checkout');
 
   setupElement(this.element, model, this);
-  bindToSubmit(this.element, model, this);
+  listenToSubmitEvent(this.element, model, this);
 }
 
-Checkout.prototype.disable = function() {
+Customer.prototype.disable = function() {
   this.element.querySelector('fieldset').setAttribute('disabled', true);
 
   return this;
 };
 
-module.exports = Checkout;
+module.exports = Customer;
 
 function setupElement(element, model, view) {
   if (element) return;
 
   view.element = domify(swig.render(template, {
-    locals: { customer: model.get('customer').toJSON() }
+    locals: { customer: model.toJSON() }
   }));
 }
 
-function bindToSubmit(element, model, view) {
+function listenToSubmitEvent(element, model, view) {
   var form = element.querySelector('form');
 
   form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    var customer = model.get('customer');
     lodash.forEach(form.elements, function(element) {
       if (!(element instanceof HTMLInputElement)) return;
 
       if (element.name.indexOf('address')) {
-        customer.set(element.name, element.value);
+        model.set(element.name, element.value);
       } else {
-        customer.setAddress(element.name.replace('address-', ''), element.value);
+        model.setAddress(element.name.replace('address-', ''), element.value);
       }
     });
 
     model.submit();
+
     view.disable();
   });
 }
