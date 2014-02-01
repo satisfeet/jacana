1319e2099b19a1a6ed73eb16c6c3ddd091027ab0
Bodo Kaiser <i@bodokaiser.io>
2014-02-01 16:07:47 +0100

    added static invoice template to app/routes/order

---
+++ app/routes/order/invoice.js
@@ -0,0 +1,14 @@
+var swig   = require('swig');
+var domify = require('domify');
+
+var template = require('../../views/order/invoice');
+
+function Invoice(element, model) {
+  this.element = element.querySelector('#invoice');
+
+  if (!this.element) {
+    this.element = domify(swig.render(template));
+  }
+}
+
+module.exports = Invoice;
