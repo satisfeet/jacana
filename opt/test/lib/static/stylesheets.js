e554ab597b9597c640c3d31c00ac893f33916001
Bodo Kaiser <i@bodokaiser.io>
2014-02-04 16:40:31 +0100

    fixed missing changes to run through tests
    - update transforms in bin/engine
    - remove "prompt" require from bin/models
    - remove send mail from lib/routes/orders
    - rename static GET of bootstrap*.css to build.css

--- opt/test/lib/static/stylesheets.js
+++ opt/test/lib/static/stylesheets.js
@@ -4,12 +4,8 @@ module.exports = function(app) {
 
   describe('/stylesheets', function() {
 
-    it('bootstrap.css', function(done) {
-      supertest(app).get('/stylesheets/bootstrap.css').expect(200, done);
-    });
-
-    it('boostrap-theme.css', function(done) {
-      supertest(app).get('/stylesheets/bootstrap-theme.css').expect(200, done);
+    it('build.css', function(done) {
+      supertest(app).get('/stylesheets/build.css').expect(200, done);
     });
 
   });
