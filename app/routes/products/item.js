eeb3924e2e9a023a2dafa1586a36f600ea696a4c
Bodo Kaiser <i@bodokaiser.io>
2014-02-05 17:50:49 +0100

    removed order button from product item
    The product item view just should give a basic overview. Everything
    further than this is too much. If we have an order button directly on
    the item we loose the ability to select size and quantity.

--- app/routes/products/item.js
+++ app/routes/products/item.js
@@ -6,16 +6,6 @@ function ProductItem(model) {
   this.element = domify(template({
     product: model.toJSON()
   }));
-
-  listenToClickEvent(this.element, model, this);
 }
 
 module.exports = ProductItem;
-
-function listenToClickEvent(element, model, view) {
-  element.querySelector('button').addEventListener('click', function(e) {
-    e.preventDefault();
-
-    model.order();
-  });
-}
